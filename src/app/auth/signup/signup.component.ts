4852810a26ec6eb223562a9f30de8650ec465a0c
Tsvetomir Dochev <tddochev@gmail.com>
2017-08-20 10:45:35 +0300

    added error messages to the form

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -11,28 +11,41 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+  alert: IAlert;
+  anAlert: boolean;
   constructor(private AngularFA: AngularFireAuth) { }
-
     ngOnInit() {
+      this.anAlert = false;
+      this.alert = {
+        type: '',
+        message: '',
+      };
     }
 
     onSignUp(form: NgForm) {
       const email = form.value.email;
       const password = form.value.password;
-      this.AngularFA.auth.createUserWithEmailAndPassword(email, password).catch(function (error: any) {
+      this.AngularFA.auth.createUserWithEmailAndPassword(email, password).catch((error: any) => {
         // Handle Errors here.
         const errorCode: string = error.code;
         const errorMessage = error.message;
-        if (errorCode === 'auth/wrong-password') {
-          alert('Wrong password.');
-          // TODO show a beautiful messsage
-        } else {
-          // TODO show a beautiful messsage
-          alert(errorMessage);
+        if (errorCode) {
+          this.anAlert = true;
+          this.alert.message = errorMessage;
+          this.alert.type = 'danger';
         }
         console.log(error);
       });
     }
+    public closeAlert(alert: IAlert) {
+      alert.message = '';
+      alert.type = '';
+      this.anAlert = false;
+    }
+}
 
+// TODO move to a models folder
+export interface IAlert {
+  type: string;
+  message: string;
 }
