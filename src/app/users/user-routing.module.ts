9f33dd2da145b7066a527ebf39e2f4280b7f80ee
Tsvetomir <tdd@outlook.com>
2017-08-29 18:35:44 +0300

    added guard to user route

--- src/app/users/user-routing.module.ts
+++ src/app/users/user-routing.module.ts
@@ -1,12 +1,13 @@
 import { RouterModule, Routes } from '@angular/router';
 
+import { AuthGuard } from './../auth/guards/auth.guard';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { UsersDetailComponent } from './../users/users-detail/users-detail.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'id', pathMatch: 'full' },
-  { path: 'id', component: UsersDetailComponent }
+  { path: 'id', component: UsersDetailComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
