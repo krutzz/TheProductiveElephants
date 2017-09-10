04bbdb62522f548145bd1b582b47f7094ab8a3f6
Tsvetomir Dochev <tddochev@gmail.com>
2017-09-10 18:40:05 +0300

    refactored sign in

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -12,22 +12,21 @@ import { NgForm } from '@angular/forms';
 export class SigninComponent implements OnInit {
   alert: Alert;
   email: string;
+  password: string;
   constructor(private AuthService: AuthService) { }
     ngOnInit() {
       this.alert = new Alert();
     }
 
     onSignIn(form: NgForm) {
-      const password = form.value.password;
-      this.AuthService.signInWithEmailAndPassword(this.email, password).then(userInfo => {
+      // const password = form.value.password;
+      this.AuthService.signInWithEmailAndPassword(this.email, this.password).then(userInfo => {
         console.log(userInfo);
       }).catch((error: any) => {
         // Handle Errors here.
         if (error) {
           this.alert.danger(error.message);
-          console.log(error);
         }
-        console.log(error);
       });
     }
     public closeAlert(alert: Alert) {
